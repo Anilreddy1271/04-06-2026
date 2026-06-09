b56be4a0ed8ac32524dccd98a0742d63140956d1
Anilreddy1271 <anil.yarrabelli@nybinfotech.com>
2026-06-09 18:22:52 +0530

    React project update 09-06-2026

--- src/functionalcomponents/A.jsx
+++ src/functionalcomponents/A.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-
-function A() {
+import React,{StrictMode} from 'react'
+import B from './B'
+function A(){
+  let a={age:10}
+  console.log(a)
   return (
-    <div>A</div>
-  );
+    <StrictMode>
+      <h1>This is A component 10</h1>
+      <h1>{a}</h1>
+      <B />
+    </StrictMode>
+  )
 }
 
-export default A;
+export default A
